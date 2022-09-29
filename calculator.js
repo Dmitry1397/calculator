909aa92ed563ad6a0e94ca06c5ec94cf6e5273cd
Dmitry1397 <dyshik8@gmail.com>
2022-09-29 09:38:36 +0200

    improove style

--- calculator.js
+++ calculator.js
@@ -12,48 +12,74 @@ const resetBtn = document.querySelector('#reset');
 
 const errorMsg = document.querySelector('#error');
 
-let a,b, action;
+let action;
+let a = b = '';
+let state = false;
 
 numbers.forEach(element => {
     element.addEventListener('click', () => {
-        output.textContent += element.value;
+        if (state) {
+            b += element.value;
+            output.textContent = parseFloat(b);    
+        } else {
+            output.textContent += element.value;
+        }
     })
 })
 
 symbols.forEach(element => {
     element.addEventListener('click', () => {
+        setDefault(symbols);
+        element.classList += ' active';
         action = element.value;
         a = output.textContent;
-        output.textContent = '';
+        state = true;
     })
 })
 
 equal.addEventListener('click', () => {
-    b = output.textContent;
-    output.textContent = actionDetect(a, b, action);
+    output.textContent = actionDetect(a, b, action, state);
+    if (state) {
+        b = '';
+    };
 })
 
 delBtn.addEventListener('click', () => output.textContent = output.textContent.slice(0, -1));
 
-resetBtn.addEventListener('click', () => output.textContent = '');
+resetBtn.addEventListener('click', () => {
+    output.textContent = '';
+    b = '';
+    setDefault(symbols);
+});
 
-function actionDetect(a, b, action) {
+function actionDetect(a, b, action, state) {
     if (action === '-') {
         errorMsg.style.display = 'none';
+        state = false;
         return parseFloat(a) - parseFloat(b);
     } else if (action === '+') {
         errorMsg.style.display = 'none';
+        state = false;
         return parseFloat(a) + parseFloat(b);
     } else if (action === '*') {
         errorMsg.style.display = 'none';
+        state = false;
         return parseFloat(a) * parseFloat(b);
     } else if (action === '/') {
         if (b === '0') {
             errorMsg.style.display = 'block';
+            state = true;
             return b;
         } else {
             errorMsg.style.display = 'none';
+            state = false;
             return parseFloat(a) / parseFloat(b);
         }
     }
+}
+
+function setDefault(array) {
+    array.forEach(element => {
+        element.classList = 'number-button';
+    })
 }
